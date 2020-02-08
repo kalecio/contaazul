1b0efb899d42ece9c64f32dc5d0f960b046c3da2
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2020-02-08 16:06:41 -0200

    configuração para novas funções

--- views/login.php
+++ views/login.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Conta Azul</title>
+        <title>Conta Azuls</title>
         <link href="<?php echo BASE_URL; ?>/assets/css/style.css" rel="stylesheet">
     </head>
 
