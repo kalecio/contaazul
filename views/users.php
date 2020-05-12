b25647a4c64c05b5af5e7fe48543793881156755
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2020-05-12 11:35:39 -0300

    inserindo nova tela de users para adicionar listar os usuários cadastrados em banco

--- views/users.php
+++ views/users.php
@@ -1,11 +1,22 @@
 <h1>Usuários</h1>
-<div class="button"><a href="<?php echo BASE_URL; ?>/users/add"> Adicionar Usuário</a> </div>
+<div class="button"><button type="button"><a href="<?php echo BASE_URL; ?>/users/add">Adicionar Usuário</a> </button></div>
+<!--  LEBMRAR DE POR UM LINK VIA JS PARA MELHORAR O USO DO BOTÃO
+* OLHAR EM  MEUS RESUMOS-->
 
 <table border="0" width="100%">
     <tr>
-        <th>Usuários</th>
-        <th width="70%">Ações</th>
+        <th>E-mail</th>
+        <th>Grupo de Permissões</th>
+        <th>Ações</th>
     </tr>
-    
+    <?php foreach ($users_list as $us) : ?>
+        <tr>
+            <td><?php echo $us['email']; ?></td>
+            <td width="200"><?php echo $us['name']; ?></td>
+            <td width="160">
+                <div class="button button_small"><a href="<?php echo BASE_URL; ?>/users/edit/<?php echo $us['id']; ?>">Editar</a></div>
+                <div class="button button_small"><a href="<?php echo BASE_URL; ?>/users/delete/<?php echo $us['id']; ?>" onclick="return confirm('Tem certeza que deseja excluir?')">Excluir</a></div>
+            </td>
+        </tr>
+    <?php endforeach; ?>
 </table>
-</div>
