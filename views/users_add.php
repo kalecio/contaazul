ba06f8fa9e2746fc3f810298176a87c942bde180
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2020-05-12 11:41:19 -0300

    implementando tela de adicionar novos usuários

---
+++ views/users_add.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adicionar</title>
+</head>
+
+<body>
+    <h1>Usuário - Adicionar</h1>
+
+    <form method="post">
+        <label> Email<br>
+            <input type="email" name="email" />
+        </label><br><br>
+        <label> Senha<br>
+            <input type="password" name="password" />
+        </label><br><br>
+        <label for="group">Grupo de Permissõess</label>
+        <select name="group" id="group">
+            <?php foreach ($group_list as $group) : ?>
+                <option value="<?php echo $group['id']; ?>"> <?php echo $group['name']; ?>
+
+                </option>
+            <?php endforeach ?> 
+        </select> <br> <br>
+
+        <input type="submit" value="Adicionar">
+    </form>
+</body>
+
+</html>
