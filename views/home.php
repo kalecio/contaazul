3be8e6074df1d44a4f01477227c96fb3727668a4
saviogomesdasilva@yahoo.com <saviogomesdasilva@yahoo.com>
2020-08-18 11:43:51 -0300

    configurando partes da home

--- views/home.php
+++ views/home.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conta Azul</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Conta Azul</title>
+    </head>
 
-<body>
-    <?php echo $company_name; ?>
-    <!-- Retorna o nome da empresa vigente --->
-    <h2>Home do Sistema</h2>
-</body>
+    <body>
+        <?php echo $company_name; ?>
+        <!-- Retorna o nome da empresa vigente --->
+        <h2>Home do Sistema</h2>
+    </body>
 
 </html>
