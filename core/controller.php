98d46bbeda49d39b835f34c1f767e544b15ac3bc
saviogomesdasilva@yahoo.com <saviogomesdasilva@yahoo.com>
2020-08-19 11:38:00 -0300

    configurando controler de proteção de dados

--- core/controller.php
+++ core/controller.php
@@ -1,29 +1,26 @@
 <?php
-class controller
-{
 
-	protected $db;
+class controller {
 
-	public function __construct()
-	{
-		global $config;
-		$this->db = new PDO("mysql:dbname=" . $config['dbname'] . ";host=" . $config['host'], $config['dbuser'], $config['dbpass']);
-	}
+    protected $db;
 
-	public function loadView($viewName, $viewData = array())
-	{
-		extract($viewData);
-		include 'views/' . $viewName . '.php';
-	}
+    public function __construct() {
+        global $config;
+        $this->db = new PDO("mysql:dbname=" . $config['dbname'] . ";host=" . $config['host'], $config['dbuser'], $config['dbpass']);
+    }
 
-	public function loadTemplate($viewName, $viewData = array())
-	{
-		include 'views/template.php';
-	}
+    public function loadView($viewName, $viewData = array()) {
+        extract($viewData);
+        include 'views/' . $viewName . '.php';
+    }
 
-	public function loadViewInTemplate($viewName, $viewData)
-	{
-		extract($viewData);
-		include 'views/' . $viewName . '.php';
-	}
+    public function loadTemplate($viewName, $viewData = array()) {
+        include 'views/template.php';
+    }
+
+    public function loadViewInTemplate($viewName, $viewData) {
+        extract($viewData);
+        include 'views/' . $viewName . '.php';
+    }
+
 }
