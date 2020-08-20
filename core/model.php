58ac52ffd659a37335d563d12f565d5a97d57f39
saviogomesdasilva@yahoo.com <saviogomesdasilva@yahoo.com>
2020-08-20 11:37:18 -0300

    configurando visualizações sobre o metodo contrutor do git

--- core/model.php
+++ core/model.php
@@ -1,12 +1,12 @@
 <?php
-class model
-{
 
-	protected $db;
+class model {
 
-	public function __construct()
-	{
-		global $config;
-		$this->db = new PDO("mysql:dbname=" . $config['dbname'] . ";host=" . $config['host'], $config['dbuser'], $config['dbpass']);
-	}
+    protected $db;
+
+    public function __construct() {
+        global $config;
+        $this->db = new PDO("mysql:dbname=" . $config['dbname'] . ";host=" . $config['host'], $config['dbuser'], $config['dbpass']);
+    }
+
 }
