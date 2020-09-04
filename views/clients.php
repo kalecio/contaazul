b4fedac461932dadffb83626bdab540c3e054520
saviogomesdasilva@yahoo.com <saviogomesdasilva@yahoo.com>
2020-09-03 21:30:33 -0300

    foi configurado e ajustado o layout css para pagina

--- views/clients.php
+++ views/clients.php
@@ -1,34 +1,44 @@
-<h1>Clientes</h1>
-<?php if ($edit_permission) : //FUNÇÃO PARA VERIFICAR SE O USUÁRIO TEM PERMISSÃO DE ADICIONAR NOVOS CLIENTES
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Painel - <?php echo $viewData['company_name'] ?></title>
+    <link href="<?php echo BASE_URL; ?>/assets/css/template.css" rel="stylesheet">
+    <script type="text/javascript" src="<?php echo BASE_URL; ?>/assets/js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="<?php echo BASE_URL; ?>/assets/js/script.js"></script>
+</head>
+
+<body>
+    <h1>Clientes</h1>
+    <?php if ($edit_permission) : //FUNÇÃO PARA VERIFICAR SE O USUÁRIO TEM PERMISSÃO DE ADICIONAR NOVOS CLIENTES
     ?>
-    <div class="button"><a href="<?php echo BASE_URL; ?>/clients/add">Adicionar Clientes</a> </div>
-<?php endif; ?>
+        <div class="button"><a href="<?php echo BASE_URL; ?>/clients/add">Adicionar Clientes</a> </div>
+    <?php endif; ?>
 
-<!--  LEBMRAR DE POR UM LINK VIA JS PARA MELHORAR O USO DO BOTÃO
-* OLHAR EM  MEUS RESUMOS-->
-
-<table border="0" width="100%">
-    <tr>
-        <th>NOME</th>
-        <th>TELEFONE</th>
-        <th>CIDADE</th>
-        <th>ESTRELAS</th>
-        <th colspan="2">Ações</th>
-    </tr>
-    <?php foreach ($clients_list as $clients) : ?>
+    <table border="0" width="100%">
         <tr>
-            <td><?php echo $clients['name']; ?></td>
-            <td><?php echo $clients['phone']; ?></td>
-            <td><?php echo $clients['address_city']; ?></td>
-            <td><?php echo $clients['stars']; ?></td>
-
-            <td width="100%">
-                <div class="button button_small"><a href="<?php echo BASE_URL; ?>/clients/edit/<?php echo $clients['id']; ?>">Alterar</a></div>
-
-            </td>
-            <td width="100%">
-                <div class="button button_small"><a href="<?php echo BASE_URL; ?>/clients/delete/<?php echo $clients['id']; ?>" onclick="return confirm('Tem certeza que deseja excluir?')">Deletar</a></div>
-            </td>
+            <th>NOME</th>
+            <th>TELEFONE</th>
+            <th>CIDADE</th>
+            <th>ESTRELAS</th>
+            <th colspan="2">Ações</th>
         </tr>
-    <?php endforeach; ?>
-</table><!-- LEMBRAR DE POR RESPOSIVIDAE DENTRO DAS VIEWS DO SISTEMA web-->
+        <?php foreach ($clients_list as $clients) : ?>
+            <tr>
+                <td><?php echo $clients['name']; ?></td>
+                <td><?php echo $clients['phone']; ?></td>
+                <td><?php echo $clients['address_city']; ?></td>
+                <td><?php echo $clients['stars']; ?></td>
+
+                <td width="100%">
+                    <div class="button button-Editar"><a href="<?php echo BASE_URL; ?>/clients/edit/<?php echo $clients['id']; ?>">Alterar</a></div>
+
+                    <div class="button button-Excluir"><a href="<?php echo BASE_URL; ?>/clients/delete/<?php echo $clients['id']; ?>" onclick="return confirm('Tem certeza que deseja excluir?')">Deletar</a></div>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table><!-- LEMBRAR DE POR RESPOSIVIDAE DENTRO DAS VIEWS DO SISTEMA web-->
+</body>
+
+</html>
