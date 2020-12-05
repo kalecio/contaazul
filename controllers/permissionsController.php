d3a572ed97544399b3a0b22e3a6045df50f48ebb
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2020-12-05 08:42:15 -0300

    configurando codigo para permissions

--- controllers/permissionsController.php
+++ controllers/permissionsController.php
@@ -1,6 +1,6 @@
 <?php
 
-class PermissionsController extends controller
+class PermissionsController extends Controller
 {
     public function __construct() /* função para importa o banco de dados */
     {
