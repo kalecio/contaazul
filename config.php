3d8d5ca8c32f1e2d6f5d46f9b0622daf38b1f367
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2020-12-17 07:13:53 -0300

    configurando projeto

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ require 'environment.php';
 global $config;
 $config = array();
 if (ENVIRONMENT == 'development') {
-	$config['dbname'] = 'conta_azul';
+	$config['dbname'] = 'contaazul';
 	$config['host'] = 'localhost';
 	$config['dbuser'] = 'root';
 	$config['dbpass'] = '';
