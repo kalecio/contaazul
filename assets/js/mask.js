6c3a39d49ded98fd5b2dccffa47a21fb33bbde3e
Sávio gomes da Silva <saviogomesdasilva@yahoo.com>
2021-01-06 22:09:05 -0300

    adicionando mascaras para campos de CEP e TELEFONE

--- assets/js/mask.js
+++ assets/js/mask.js
@@ -1,4 +1,20 @@
+// TRATAMENTO PARA QUESTÕES DE NUMERO DE CELULAR
+jQuery("input.phone")
+    .mask("(99) 9.9999-9999")
+    .focusout(function (event) {
+        var target, phone, element;
+        target = (event.currentTarget) ? event.currentTarget : event.srcElement;
+        phone = target.value.replace(/\D/g, '');
+        element = $(target);
+        element.unmask();
+        if (phone.length > 10) {
+            element.mask("(99) 9.9999-9999");
+        } else {
+            element.mask("(99) 9.9999-9999");
+        }
+    });
+
 
 $(document).ready(function () {
-    $("#cep").mask("99.999-999");
+    $("#address_zipcode").mask("99.999-999");
 });
