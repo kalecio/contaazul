69129a49d62193b855b3830ef0ec2a197190f2c7
Sávio gomes da Silva <saviogomesdasilva@yahoo.com>
2021-01-06 22:09:32 -0300

    criando requisição Ajax para busca por nomes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-$(function() {
-    $('.tabitem').on('click', function() {
+$(function () {
+    $('.tabitem').on('click', function () {
         $('.activetab').removeClass('activetab');
         $(this).addClass('activetab');
 
@@ -8,4 +8,42 @@ $(function() {
         $('.tabbody').eq(item).show();
     });
     //$('.tabbody').eq(0).show(); para modificação simples
+    $('#busca').on('focus', function () {
+        $(this).animate({
+            width: '250px'
+        });
+        $('#busca').on('blur', function () {
+            if ($(this).val() == '') {
+                $(this).animate({
+                    width: '100px'
+                });
+            }
+        });
+
+        $('#busca').on('keyup', function () {
+            var datatype = $(this).attr('data-type');
+            var busca = $(this).val();
+
+
+            if (datatype != '') {
+                $.ajax({
+                    url: BASE_URL + '/ajax/' + datatype,
+                    type: 'GET',
+                    data: { busca: busca },
+                    dataType: 'json',
+                    success: function (json) {
+
+
+                    }
+
+
+                });
+
+            }
+
+        });
+    });
+
 });
+
+
