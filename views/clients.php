d04d0d2a5a1eda11dba47d4bb7dfbc7db4117dfe
Sávio gomes da Silva <saviogomesdasilva@yahoo.com>
2021-01-06 22:11:10 -0300

    adicionando mascaras e formatação

--- views/clients.php
+++ views/clients.php
@@ -13,41 +13,44 @@
     <h1>Clientes</h1>
     <?php if ($edit_permission) : //FUNÇÃO PARA VERIFICAR SE O USUÁRIO TEM PERMISSÃO DE ADICIONAR NOVOS CLIENTES
     ?>
-        <div class="button"><a href="<?php echo BASE_URL; ?>/clients/add">Adicionar Clientes</a> </div>
-    <?php endif; ?>
+        <div>
+            <div class="button"><a href="<?php echo BASE_URL; ?>/clients/add">Adicionar Clientes</a> </div>
+        <?php endif; ?>
+        <input type="text" id="busca" data-type="search_clients" />
+        <br />
+        </div>
+        <br />
+        <div>
+            <table border="0" width="100%">
+                <tr>
+                    <th>NOME</th>
+                    <th>TELEFONE</th>
+                    <th>CIDADE</th>
+                    <th>ESTRELAS</th>
+                    <th colspan="2">Ações</th>
+                </tr>
+                <?php foreach ($clients_list as $clients) : ?>
+                    <tr>
+                        <td><?php echo $clients['name']; ?></td>
+                        <td><?php echo $clients['phone']; ?></td>
+                        <td><?php echo $clients['address_city']; ?></td>
+                        <td><?php echo $clients['stars']; ?></td>
 
-    <table border="0" width="100%">
-        <tr>
-            <th>NOME</th>
-            <th>TELEFONE</th>
-            <th>CIDADE</th>
-            <th>ESTRELAS</th>
-            <th colspan="2">Ações</th>
-        </tr>
-        <?php foreach ($clients_list as $clients) : ?>
-            <tr>
-                <td><?php echo $clients['name']; ?></td>
-                <td><?php echo $clients['phone']; ?></td>
-                <td><?php echo $clients['address_city']; ?></td>
-                <td><?php echo $clients['stars']; ?></td>
+                        <td width="100%">
+                            <div class="button button-Editar"><a href="<?php echo BASE_URL; ?>/clients/edit/<?php echo $clients['id']; ?>">Alterar</a></div>
 
-                <td width="100%">
-                    <div class="button button-Editar"><a href="<?php echo BASE_URL; ?>/clients/edit/<?php echo $clients['id']; ?>">Alterar</a></div>
-
-                    <div class="button button-Excluir"><a href="<?php echo BASE_URL; ?>/clients/delete/<?php echo $clients['id']; ?>" onclick="return confirm('Tem certeza que deseja excluir?')">Deletar</a></div>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    </table><!-- LEMBRAR DE POR RESPOSIVIDAE DENTRO DAS VIEWS DO SISTEMA web-->
-    <div class="pagination">
-<?php for($q=1;$q<=$p_count;$q++): ?>
-	<div class="pag_item <?php echo ($q==$p)?'pag_ativo':''; ?>"><a href="<?php echo BASE_URL; ?>/clients?p=<?php echo $q; ?>"><?php echo $q; ?></a></div>
-<?php endfor; ?>
-<div style="clear:both"></div>
-</div>
-
-    
-
+                            <div class="button button-Excluir"><a href="<?php echo BASE_URL; ?>/clients/delete/<?php echo $clients['id']; ?>" onclick="return confirm('Tem certeza que deseja excluir?')">Deletar</a></div>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div><!-- LEMBRAR DE POR RESPOSIVIDAE DENTRO DAS VIEWS DO SISTEMA web-->
+        <div class="pagination">
+            <?php for ($q = 1; $q <= $p_count; $q++) : ?>
+                <div class="pag_item <?php echo ($q == $p) ? 'pag_ativo' : ''; ?>"><a href="<?php echo BASE_URL; ?>/clients?p=<?php echo $q; ?>"><?php echo $q; ?></a></div>
+            <?php endfor; ?>
+            <div style="clear:both"></div>
+        </div>
 </body>
 
 </html>
