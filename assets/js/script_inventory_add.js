d54f31523a05969268b34cd6018b64d50160c8df
Sávio gomes da Silva <saviogomesdasilva@yahoo.com>
2021-01-16 16:30:44 -0300

    cirando nova mascara para campos do inventory

--- assets/js/script_inventory_add.js
+++ assets/js/script_inventory_add.js
@@ -1,5 +1,5 @@
-$(function(){
+$(function () {
 
-	$('input[name=price]').mask('000.000.000.000.000,00', {reverse:true, placeholder:"0,00"});
+	$('input[name=price]').mask('000.000.000.000.000,00', { reverse: true, placeholder: "0,00" });
 
 });
