e5d2af0d68232d2185ed8beb84e8f11935ece580
Sávio gomes da Silva <saviogomesdasilva@yahoo.com>
2021-01-16 16:31:30 -0300

    configurando funcionalidades da controler do sistema

--- controllers/inventoryController.php
+++ controllers/inventoryController.php
@@ -21,9 +21,31 @@ class InventoryController extends Controller
         $data['user_email'] = $user->getEmail();
 
         if ($user->hasPermission('inventory_view')) {
+            $inventory = new InventoryModel();
+            $offset = 0;
+
+
+            $data['inventory_list'] = $inventory->getList($offset, $user->getCompany());
+
+            $data['add_permission'] = $user->hasPermission('inventory_add');
+            $data['edit_permission'] = $user->hasPermission('inventory_edit');
+            $this->loadTemplate('inventory', $data);
         } else {
 
             header("Location:" . BASE_URL);
+        }
+    }
+    public function add()
+    {
+        $data = array();
+        $user = new UsersModels();
+        $user->setLoggedUser();
+        $company = new CompaniesModels($user->getCompany());
+        $data['company_name'] = $company->getName();
+        $data['user_email'] = $user->getEmail();
+
+        if ($user->hasPermission('inventory_add')) {
+            $this->loadTemplate('inventory_add', $data);
         }
     }
 }
