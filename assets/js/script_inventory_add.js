e9b14d0c1e13f655af2377b2564d91d0393b3c2b
Sávio gomes da Silva <saviogomesdasilva@yahoo.com>
2021-02-18 15:59:35 -0300

    configurando botao para tela

--- assets/js/script_inventory_add.js
+++ assets/js/script_inventory_add.js
@@ -1,5 +1,5 @@
 $(function () {
 
-	$('input[name=price]').mask('000.000.000.000.000,00', { reverse: true, placeholder: "0,00" });
+	$('input[name=price]').mask('000.000.000.000.000,00', { reverse: true, placeholder: "00,00" });
 
 });
