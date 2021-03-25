f7c5db698418b8a62d0488c38dcf1753bfd4e29e
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2021-03-25 10:38:23 -0300

    criando nova tela de login para o sistema, em modo responsivo

--- views/login.php
+++ views/login.php
@@ -1,52 +1,84 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+
     <title>Sistema Conta Azul</title>
-    <link href="<?php echo BASE_URL; ?>/assets/css/login.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="assets/css/styles.css" rel="stylesheet"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js"
+            crossorigin="anonymous"></script>
 </head>
-
-<body>
-    <div id="login">
-        <h3 class="text-center text-white pt-5">LFormulário de login</h3>
-        <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <form method="post" class="form">
-                            <h3 class="text-center text-info">Sistema Conta azul</h3>
-                            <div class="form-group">
-                                <label for="username" class="text-info">Usuário:</label><br>
-                                <input type="email" class="form-control" name="email" placeholder="Digite seu email padrão">
-
+<body class="bg-primary">
+<div id="layoutAuthentication">
+    <div id="layoutAuthentication_content">
+        <main>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5">
+                        <div class="card shadow-lg border-0 rounded-lg mt-5">
+                            <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                            <div class="card-body">
+                                <form method="post" class="form">
+                                    <div class="form-group">
+                                        <label class="small mb-1" for="inputEmailAddress">Email</label>
+                                        <input class="form-control py-4" id="inputEmailAddress" type="email"
+                                               name="email" placeholder="Enter email address"/>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="small mb-1" for="inputPassword">Password</label>
+                                        <input class="form-control py-4" id="inputPassword" type="password"
+                                               name="password" placeholder="Enter password"/>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox">
+                                            <input class="custom-control-input" id="rememberPasswordCheck"
+                                                   type="checkbox"/>
+                                            <label class="custom-control-label" for="rememberPasswordCheck">Remember
+                                                password</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
+                                        <a class="small" href="password.html">Forgot Password?</a>
+                                        <!-- COMANDO PHP PARA EMITIR ERRO CASO NÃO ESTEJA CADASTRADO EM SISTEMA-->
+                                        <?php if (isset($error) && !empty($error)) : ?>
+                                            <div class="alert alert-warning" role="alert"><?php echo $error; ?></div>
+                                        <?php endif; ?>
+                                        <br>
+                                        <button type="submit" class="btn btn-primary">Entrar</button>
+                                    </div>
+                                </form>
                             </div>
-                            <div class="form-group">
-                                <label class="text-info">Senha de Acesso:</label><br>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha" name="password">
-
+                            <div class="card-footer text-center">
+                                <div class="small"><a href="register.html">Need an account? Sign up!</a></div>
                             </div>
-                            <!-- COMANDO PHP PARA EMITIR ERRO CASO NÃO ESTEJA CADASTRADO EM SISTEMA-->
-                            <?php if (isset($error) && !empty($error)) : ?>
-                                <div class="alert alert-warning" role="alert"><?php echo $error; ?></div>
-                            <?php endif; ?>
-                            <br>
-                            <button type="submit" class="btn btn-primary">Entrar</button>
-
-                        </form>
-                        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-                        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-                        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
     </div>
+    <div id="layoutAuthentication_footer">
+        <footer class="py-4 bg-light mt-auto">
+            <div class="container-fluid">
+                <div class="d-flex align-items-center justify-content-between small">
+                    <div class="text-muted">Copyright &copy; Your Website 2020</div>
+                    <div>
+                        <a href="#">Privacy Policy</a>
+                        &middot;
+                        <a href="#">Terms &amp; Conditions</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
+<script src="assets/js/scripts.js"></script>
 </body>
-
 </html>
