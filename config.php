ba03201b20d680c581d39d0848fbe797bc629fb9
=Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2021-04-26 17:37:30 -0300

    configurando try-catch

--- config.php
+++ config.php
@@ -1,21 +1,22 @@
 <?php
+
 require 'environment.php';
 
 global $config;
-$config = array();
+$config = [];
 try {
-	if (ENVIRONMENT == 'development') {
-		$config['dbname'] = 'conta_azul';
-		$config['host'] = 'localhost';
-		$config['dbuser'] = 'root';
-		$config['dbpass'] = '';
-	} else {
-		$config['dbname'] = 'conta_azul';
-		$config['host'] = '192.168.1.30';
-		$config['dbuser'] = 'root';
-		$config['dbpass'] = '';
-	}
-	//code...
+    if (ENVIRONMENT == 'development') {
+        $config['dbname'] = 'conta_azul';
+        $config['host'] = 'localhost';
+        $config['dbuser'] = 'root';
+        $config['dbpass'] = '';
+    } else {
+        $config['dbname'] = 'conta_azul';
+        $config['host'] = '192.168.1.30';
+        $config['dbuser'] = 'root';
+        $config['dbpass'] = '';
+    }
+    //code...
 } catch (\Throwable $th) {
-	echo 'Erro ao acessar sistema', $th->getMessage();
+    echo 'Erro ao acessar sistema', $th->getMessage();
 }
