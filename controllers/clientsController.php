7ea6040a10ea894dfac4a18aa4f48c58c8b3955f
=Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2021-04-26 14:37:18 -0300

    configurando novas variaveis

--- controllers/clientsController.php
+++ controllers/clientsController.php
@@ -13,14 +13,14 @@ class ClientsController extends Controller
 
     public function index() {
     	$data = array();
-    	$u = new UsersModels();
-        $u->setLoggedUser();
-        $company = new Companies($u->getCompany());
+    	$user = new UsersModels();
+        $user->setLoggedUser();
+        $company = new CompaniesModels($user->getCompany());
         $data['company_name'] = $company->getName();
-        $data['user_email'] = $u->getEmail();
+        $data['user_email'] = $user->getEmail();
 
-        if($u->hasPermission('clients_view')) {
-            $c = new Clients();
+        if($user->hasPermission('clients_view')) {
+            $clients = new ClientsModels();
             $offset = 0;
             $data['p'] = 1;
             if(isset($_GET['p']) && !empty($_GET['p'])) {
@@ -31,10 +31,10 @@ class ClientsController extends Controller
             }
             $offset = ( 10 * ($data['p']-1) );
 
-            $data['clients_list'] = $c->getList($offset, $u->getCompany());
-            $data['clients_count'] = $c->getCount($u->getCompany());
+            $data['clients_list'] = $clients->getList($offset, $user->getCompany());
+            $data['clients_count'] = $clients->getCount($user->getCompany());
             $data['p_count'] = ceil( $data['clients_count'] / 10 );
-            $data['edit_permission'] = $u->hasPermission('clients_edit');
+            $data['edit_permission'] = $user->hasPermission('clients_edit');
 
     		$this->loadTemplate('clients', $data);
     	} else {
