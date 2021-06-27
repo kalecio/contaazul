dcf997995d08d90c12948edfe7ce598bf2c849f4
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2021-06-27 16:26:55 -0300

    configurando projeto

--- config.php
+++ config.php
@@ -9,12 +9,12 @@ try {
         $config['dbname'] = 'conta_azul';
         $config['host'] = 'localhost';
         $config['dbuser'] = 'root';
-        $config['dbpass'] = '12345';
+        $config['dbpass'] = '123456789';
     } else {
         $config['dbname'] = 'conta_azul';
         $config['host'] = '192.168.1.30';
         $config['dbuser'] = 'root';
-        $config['dbpass'] = '12345';
+        $config['dbpass'] = '123456789';
     }
     //code...
 } catch (\Throwable $th) {
